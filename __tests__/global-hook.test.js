cb12e928f1879a3cca5c1f57a871575103ca4724
RustyDev <horizens12@gmail.com>
2019-08-16 15:48:14 -0700

    Added logout button and action

--- __tests__/global-hook.test.js
+++ __tests__/global-hook.test.js
@@ -10,16 +10,25 @@ it("renders without crashing", () => {
 })
 
 it("App loads with initial state of logged out", () => {
+
   const { container } = render(<App />)
   const countValue = getByTestId(container, "idAuthenticated")
+
   expect(countValue.textContent).toBe("false")
 })
 
-it("Authenticate button should work", () => {
+it("Authenticate buttons should work", () => {
   const { container } = render(<App />)
+
   const login = getByTestId(container, "idAuthenticated")
+  const logout = getByTestId(container, "idNotAuthenticated")
+
   expect(login.textContent).toBe("false")
+
   fireEvent.click(login)
   expect(login.textContent).toBe("true")
+
+  fireEvent.click(logout)
+  expect(login.textContent).toBe("false")
 })
 
--- global.js
+++ global.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {Fragment} from "react"
 import useGlobalHook from "./useGlobalHook"
 
 const initialState = {
@@ -8,6 +8,9 @@ const initialState = {
 const actions = {
   login: (store) => {
     store.setState({ authenticated: true })
+  },
+  logout: (store) => {
+    store.setState({ authenticated: false })
   }
 }
 
@@ -17,11 +20,17 @@ function App() {
 
   const [globalState, globalActions] = useGlobal()
   const login = () => globalActions.login()
+  const logout = () => globalActions.logout()
 
   return (
-    <button onClick={login} data-testid="idAuthenticated">
-      { globalState.authenticated.toString() }
-    </button>
+    <>
+      <button onClick={login} data-testid="idAuthenticated">
+        { globalState.authenticated.toString() }
+      </button>
+      <button onClick={logout} data-testid="idNotAuthenticated">
+        { globalState.authenticated.toString() }
+      </button>
+    </>
   )
 }
 
